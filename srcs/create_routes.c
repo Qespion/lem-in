9e4f4fa5117ab92432087912cad13fd7a4b75bed
qespion <q.espion@gmail.com>
2019-02-18 18:51:59 +0100

    found solution

--- srcs/create_routes.c
+++ srcs/create_routes.c
@@ -6,12 +6,37 @@
 /*   By: oespion <oespion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 19:28:15 by oespion           #+#    #+#             */
-/*   Updated: 2019/02/17 20:12:50 by oespion          ###   ########.fr       */
+/*   Updated: 2019/02/18 18:48:55 by oespion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/libft.h"
 #include "lem_in.h"
+
+void		read_current(t_solve *solution)
+{
+	ft_printf("--current state\n");
+	while (solution)
+	{
+		ft_printf("%s\n", solution->path->current->name);
+		solution = solution->next;
+	}
+	ft_printf("------\n");
+}
+
+int		found_finish_line(t_solve *solution, t_map *map)
+{
+	while (solution)
+	{
+		if (solution->path->current == map->end)
+		{
+			ft_printf("\n\n BOYS WE FOUND SOMETHING\n--\n");
+			return (1);
+		}
+		solution = solution->next;
+	}
+	return (0);
+}
 
 int         ft_check(t_solve *routes, char *end_name)
 {
@@ -38,17 +63,29 @@ t_solve		*ft_add_end(t_solve *add_end, t_map *map, t_solve *solution)
 {
 	t_solve	*tmp_solve;
 	t_link	*tmp_link;
+	t_road	*road;
+	t_solve *new_solve;
 
 	tmp_solve = solution;
-	while (tmp_solve)
+	tmp_link = tmp_solve->path->current->link;
+	while (tmp_link)
 	{
-		tmp_link = tmp_solve->path->current->link;
-		if (tmp_link->node == tmp_solve->path->prev->current)
+		if (tmp_link->node == solution->path->prev->current)
+		{
 			tmp_link = tmp_link->next;
-		ft_printf("current %s\n", tmp_link->node->name);
-		ft_printf("prev node %s\n", tmp_solve->path->prev->current->name);
-		tmp_solve = tmp_solve->next;
-
+			continue;
+		}
+		if (!(new_solve = (t_solve*)malloc(sizeof(t_solve))))
+			exit(-1);
+		if (!(road = (t_road*)malloc(sizeof(t_road))))
+			exit(-1);
+		road->current = tmp_link->node;
+		road->prev = tmp_solve->path;
+		new_solve->next = NULL;
+		new_solve->path = road;
+		add_end->next = new_solve;
+		add_end = add_end->next;
+		tmp_link = tmp_link->next;
 	}
 	return (add_end);
 }
@@ -59,15 +96,24 @@ t_solve     *new_turn(t_solve *solution, t_map *map)
 	t_solve	*end;
 	t_solve	*add_end;
 
+	int	turn = 0;
 	tmp = solution;
 	end = solution;
 	while (end->next)
 		end = end->next;
 	add_end = end;
-	while (tmp != end)
+	while (!found_finish_line(solution, map))
 	{
-		add_end = ft_add_end(add_end, map, tmp);
-		tmp = tmp->next;
+		while (tmp != end)
+		{
+			add_end = ft_add_end(add_end, map, tmp);
+			tmp = tmp->next;
+		}
+		turn += 1;
+		ft_printf("turn : %d\n\n", turn);
+		end = add_end;
+		read_current(solution);
+		tmp = solution;
 	}
 	add_end = ft_add_end(add_end, map, tmp);
 	return solution;
@@ -75,12 +121,6 @@ t_solve     *new_turn(t_solve *solution, t_map *map)
 
 t_solve     *create_routes(t_map *map, int max_roads, t_solve * routes)
 {
-	while (42)
-	{
-		if (ft_check(routes, map->end->name))
-			break;
-		routes = new_turn(routes, map);
-		exit(-1);
-	}
+	routes = new_turn(routes, map);
 	return routes;
 }
